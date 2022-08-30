08096ad0b6b123e04478767116b9774ebaa863e2
Ashwin Venkatesh <ashwin.what@gmail.com>
2022-08-30 11:24:46 -0400

    Register mesh-gateways using the endpoints controller.
    - Use consul-dataplane to configure the mesh-gateway proxy and remove
      envoy container.
    - Remove instances of client and auto-encrypt from the deployment.

--- control-plane/connect-inject/consul_dataplane_sidecar_test.go
+++ control-plane/connect-inject/consul_dataplane_sidecar_test.go
@@ -291,7 +291,6 @@ func TestHandlerConsulDataplaneSidecar_Multiport(t *testing.T) {
 						annotationService: "web,web-admin",
 					},
 				},
-
 				Spec: corev1.PodSpec{
 					Volumes: []corev1.Volume{
 						{
